b6956f748b7fa6171396f35a2252627e8e745b1b
Daniel Alves <alves.daniel4320@gmail.com>
2025-06-20 01:41:37 -0300

    Configuração inicial

---
+++ src/test/java/com/daniel/flyshort/flyshort/FlyshortApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.flyshort.flyshort;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyshortApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
